afec1e60ee54150269712b117d15650f8f7d1027
xenleme <xenleme@gmail.com>
2018-11-22 13:23:19 +0300

    Function 2 - squaring an argument

---
+++ 8kyu/function-2-squaring-an-argument.js
@@ -0,0 +1,8 @@
+// Now you have to write a function called square that takes an argument and
+// returns the square of it.
+
+const square = (x) => { return Math.pow(x, 2) }
+
+console.log(square(7056));
+console.log(square(289));
+console.log(square(9025));
