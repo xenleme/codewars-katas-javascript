3d50e4f9971e74f9c5b2523d4b214517c15156d8
xenleme <xenleme@gmail.com>
2018-12-04 07:28:09 +0300

    Add solution to: 8 kyu - You only need one - Beginner

---
+++ 8kyu/you-only-need-one-beginner.js
@@ -0,0 +1,17 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//
+// Array can contain numbers or strings. X can be either.
+//
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  return a.includes(x);
+}
+
+console.log(check([66, 101], 66));
+
+
+
+
+
+
