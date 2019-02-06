0cdb6b1996afc742dd4bf6c25366f0198ecc12ca
xenleme <xenleme@gmail.com>
2019-02-06 07:24:42 +0300

    Add solution to: 8 kyu - String cleaning

---
+++ 8kyu/string-cleaning.js
@@ -0,0 +1,23 @@
+// Your boss decided to save money by purchasing some cut-rate optical character 
+// software for scanning in the text of old novels to your database. 
+// At first it seems to capture words okay, but you quickly notice that it 
+// throws in a lot of numbers at random places in the text. 
+// For example:
+
+// stringClean('! !') == '! !'
+// stringClean('123456789') == ''
+// stringClean('This looks5 grea8t!') == 'This looks great!'
+// Your harried co-workers are looking to you for a solution to take this 
+// garbled text and remove all of the numbers. Your program will take in a 
+// string and clean out all numeric characters, and return a string with spacing 
+// and special characters ~#$%^&!@*():;"'.,? all intact.
+
+function stringClean(s){
+  return s.replace(/[0-9]/g, '');
+}
+
+console.log(stringClean(""));
+console.log(stringClean("! !"));
+console.log(stringClean("123456789"));
+console.log(stringClean("(E3at m2e2!!)"));
+console.log(stringClean("Dsa32 cdsc34232 csa!!! 1I 4Am cool!"));
